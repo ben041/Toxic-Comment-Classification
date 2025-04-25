7bb88ca3d233de3f9a3d6c14034471310e3c9105
ben041 <benmalunga041@gmail.com>
2025-04-25 23:15:07 +0000

    real time

--- toxic_comment/core/urls.py
+++ toxic_comment/core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
 urlpatterns = [
@@ -6,6 +6,9 @@ urlpatterns = [
     path('Word by word Analysis/', views.word_by_word, name='word_by_word'),
     path('About/', views.about, name='about'),
     path('Welcome/', views.landing_page, name='landing_page'),
+
+    path('Real-time Analysis/', views.realtime_view, name='realtime'),
+    path('analyze_text/', views.analyze_text, name='analyze_text'),
 
     path('', views.login_view, name='login'),
     path('Create an Account/', views.register_view, name='register'),
